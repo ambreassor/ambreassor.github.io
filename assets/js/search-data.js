8982d2ad7f81d97820549eca0c8c2a67d1880853
ambreassor <ambreassor@users.noreply.github.com>
2025-05-20 07:45:14 +0000

    deploy: da51c459f62a2da8cdc1e79ea44792f76c405b91

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,10 +64,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-announcement-test",
-          title: 'announcement test',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
